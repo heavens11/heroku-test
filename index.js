1cac1dc0acb1b9eaa21744403f087b0a819ee961
Sourav Dey <soudey1997@gmail.com>
2025-07-14 12:55:55 +0530

    Fix encoding for emoji display

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
   res.end('🎉 Hello from Render – it works!\n');
 });
 
